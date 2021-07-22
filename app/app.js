4d7a7120ca9d2968ed5d2d1985c2c795d2ae1978
andresaponte22 <80225604+andresaponte22@users.noreply.github.com>
2021-07-22 19:19:13 -0400

    Added functions to add. Updated schema and seed. Database shows up properly with seeds

--- app/app.js
+++ app/app.js
@@ -2,6 +2,7 @@ const inquirer = require('inquirer')
 const mysql = require('mysql')
 const cTable = require('console.table')
 
+// connection
 let connection = mysql.createConnection({
   host: 'localhost',
   port: 3306,
@@ -15,6 +16,7 @@ connection.connect(function(err) {
   start()
 });
 
+// main menu
 const start = () => {
   inquirer
     .prompt([
@@ -58,10 +60,6 @@ const start = () => {
         case "Add role":
           addRole()
           break
-
-        case "Update employee role":
-          updateEmployeeRole()
-          break
         
         case "EXIT":
           connection.end();
@@ -103,23 +101,115 @@ function rolesViewAll() {
 // functions
 // adds
 function addEmployee() {
-  
-}
+  connection.query('Select * FROM role', async (err, roles) => {
+    if (err) throw err; 
+    
+    connection.query('Select * FROM employee WHERE manager_id IS NULL', async (err, managers) => {
+      if (err) throw err; 
+
+    managers = managers.map(manager => ({name:manager.first_name + " " + manager.last_name, value: manager.id}));
+    managers.push({name:"None"});
+
+    const responses = await inquirer
+      .prompt([
+        {
+          name: "first_name",
+          type: "input",
+          message: "What is the employee's first name?: "
+        },
+        {
+          name: "last_name",
+          type: "input",
+          message: "What is the employee's last name?: "
+        },
+        {
+          name: "role_id",
+          type: "list",
+          message: "What is the employee's role?: ",
+          choices: roles.map(role => ({name: role.title, value: role.id}))
+        },
+      ]).then(function(response) {
+        connection.query(
+          'INSERT INTO employee SET ?',
+          {
+            first_name: response.first_name,
+            last_name: response.last_name,
+            role_id: response.role_id
+          },
+          function(err) {
+            if (err) throw err;
+            console.log("Succes: Employee added");
+            start();
+          }
+      )
+    })
+  })
+})
+};
 
 function addDepartment() {
-  
+  connection.query("SELECT * FROM department", function(err) {
+  if (err) throw err;
+  inquirer
+  .prompt([
+    {
+      name: "departmentName",
+      type: "input",
+      message: "What is the name of the department you would like to add?: "
+    },
+  ])
+  .then(function(response) {
+    connection.query(
+      "INSERT INTO department SET ?",
+      {
+        departmentName: response.name,
+      },
+      function(err) {
+        if (err) throw err;
+        console.log("Success: Department added");
+        start();
+      }
+    );
+  });
+})
 }
 
 function addRole() {
-
-}
-
-function updateEmployeeRole() {
-
-}
+  var query = "SELECT title, salary FROM role INNER JOIN department ON role.department_id = department.department_id;";
+    connection.query(query, function(err, results) {
+      if (err) throw err;
+    inquirer
+    .prompt([
+      {
+        name: "roleName",
+        type: "input",
+        message: "What is the name of the role you would like to add?: "
+      },
+      {
+        name: "roleSalary",
+        type: "input",
+        message: "What is the salary for this role?: ",
+      },
+    ])
+    .then(function(response) {
+      connection.query(
+        "INSERT INTO role SET ?",
+        {
+          title_name: response.roleName,
+          salary: response.roleSalary,
+        },
+        function(err) {
+          if (err) throw err;
+          console.log("Success: Role created succesfully.")
+          start();
+        }
+      )
+    })
+  })
+};
 
 connection.connect((err) => {
   if (err) throw err;
-  console.log(`connected as id ${connection.threadId}`);
+  console.log(`Connected as id ${connection.threadId}`);
   start();
 });
--- app/schema.sql
+++ app/schema.sql
@@ -12,6 +12,12 @@ CREATE TABLE employee (
   PRIMARY KEY (employee_id)
 ); 
 
+CREATE TABLE department (
+  department_id INT NOT NULL AUTO_INCREMENT,
+  name VARCHAR(30) NOT NULL,
+  PRIMARY KEY (department_id)
+);
+
 CREATE TABLE role (
   role_id INT NOT NULL AUTO_INCREMENT,
   title VARCHAR(30) NOT NULL, 
@@ -20,8 +26,11 @@ CREATE TABLE role (
   PRIMARY KEY (role_id)
 );
 
-CREATE TABLE department (
-  department_id INT NOT NULL AUTO_INCREMENT,
-  name VARCHAR(30) NOT NULL,
-  PRIMARY KEY (department_id)
-);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Andres", "Aponte", 1, 1), ("Alfonso", "Aponte", 2, 2), ("Orlando", "Gray", 3, 1), ("John", "Dasilva", 4, 1);
+
+INSERT INTO department(department_id, name)
+VALUES (1, "Sales"), (2, "Software"), (3, "Stats"), (4, "Legal");
+
+INSERT INTO role(title, salary, department_id)
+VALUES ("Sales Lead", 60000, 1), ("Front End", 80000, 2), ("Lead Stats", 100000, 3), ("Software Engineer", 100000, 2), ("Lawyer", 200000, 4);
